22aa374212bd8eda8b376f44715679539a1e017e
totallylegitperson.example@gmail.com <totallylegitperson.example@gmail.com>
2023-07-21 11:22:53 -0400

    create script folder and networkUI script

---
+++ GIthubCollab/Assets/Script/NetworkManagerUI.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NetworkManagerUI : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] private Button ServerButton;
+    [SerializeField] private Button HostButton;
+    [SerializeField] private Button ClientButton;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
