6b37a260fe5426705eecb5d00fc5059ceeb1fc43
totallylegitperson.example@gmail.com <totallylegitperson.example@gmail.com>
2023-07-24 12:15:35 -0400

    create basic network variables

--- GIthubCollab/Assets/Script/PlayerNetwork.cs
+++ GIthubCollab/Assets/Script/PlayerNetwork.cs
@@ -1,27 +1,77 @@
-  using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
+using Unity.Collections;
 using Unity.Netcode;
 using UnityEngine;
 
 public class PlayerNetwork : NetworkBehaviour
 {
-    //private NetworkVariable<int> randomNumber;
+    //private NetworkVariable<int> randomNumber = new NetworkVariable<int>(1, NetworkVariableReadPermission.Everyone, NetworkVariableWritePermission.Owner);
+    public struct NetworkData : INetworkSerializable
+    {
+        public int _int;
+        public bool _bool;
+        public FixedString32Bytes _keyPressed;
+        public void NetworkSerialize<T>(BufferSerializer<T> serializer) where T : IReaderWriter
+        {
+            serializer.SerializeValue(ref _int);
+            serializer.SerializeValue(ref _bool);
+            serializer.SerializeValue(ref _keyPressed);
 
+        }
+    }
+    private NetworkVariable<NetworkData> randomNumber = new NetworkVariable<NetworkData>(
+         new NetworkData{
+             _int = 56,
+             _bool = true,
+             _keyPressed = "none",
+         },
+         NetworkVariableReadPermission.Everyone, NetworkVariableWritePermission.Owner
+        
+        );
     // Start is called before the first frame update
     void Start()
     {
         
     }
-
+    public override void OnNetworkSpawn()
+    {
+        randomNumber.OnValueChanged += (NetworkData previousValue, NetworkData newValue) =>
+        {
+            Debug.Log(OwnerClientId + " ; " + randomNumber.Value._int + randomNumber.Value._bool + randomNumber.Value._keyPressed);
+        };
+    } 
     // Update is called once per frame
     void Update()
     {
-        if (!IsOwner) return;
+
+        
+
+        if (!IsOwner) return; // if machine is owner of object then they can control it (pass this point)
         Vector3 moveDir = Vector3.zero;
         if (Input.GetKey(KeyCode.W)) moveDir.y = +1f;
         if (Input.GetKey(KeyCode.S)) moveDir.y = -1f;
         if (Input.GetKey(KeyCode.A)) moveDir.x = -1f;
         if (Input.GetKey(KeyCode.D)) moveDir.x = +1f;
+        if (Input.GetKey(KeyCode.R))
+        {
+            randomNumber.Value = new NetworkData {
+                _int = Random.Range(0, 100),
+                _bool = false,
+
+            };            
+        }
+        if (Input.anyKeyDown)
+        {
+            randomNumber.Value = new NetworkData
+            {
+                _int = randomNumber.Value._int.
+                _bool = randomNumber.Value._bool,
+                _keyPressed = Input.GetKeyDown
+
+            };
+        }
+
 
         float movesSpeed = 3f;
         transform.position += moveDir * movesSpeed * Time.deltaTime;  
